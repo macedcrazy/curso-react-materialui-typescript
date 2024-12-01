56255f87896786bf11df83499f286c74e2f23e88
Anderson Macedo <macedcrazy@gmail.com>
2024-12-01 08:15:06 -0300

    feat: Adicionado tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { LightTheme } from "./shared/themes";
 
 
 
 export const App = () => {
   return (
+    <ThemeProvider theme={LightTheme}>
     <BrowserRouter>
        <AppRoutes/>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 

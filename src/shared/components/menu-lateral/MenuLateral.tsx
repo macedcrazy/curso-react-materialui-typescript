0116b05e62419c4890ab6ef50a4ce2b0803dcfd5
Anderson Macedo <macedcrazy@gmail.com>
2024-12-05 17:37:08 -0300

    feat: configuração do menu

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,42 +1,71 @@
 import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material";
 import { useDrawerContext } from "../../contexts";
+import { useMatch, useNavigate, useResolvedPath } from "react-router-dom";
 
 
+interface IListItemLinkProps {
+  icon: string;
+  to: string;
+  label: string;
+  onClick?: () => void;  // Tornando `onClick` opcional, sem necessidade de `undefined`
+}
+
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.();
+  }
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  )
+}
 
 
 interface iMenuLateralprops {
   children: React.ReactNode;
-
 }
 
 export const MenuLateral: React.FC<iMenuLateralprops> = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const {isDrawerOpen , toggleDrawerOpen } = useDrawerContext();
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
 
   return (
     <>
-      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'}onClose={toggleDrawerOpen}>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
-
-          <Box width='100%' height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center' >
-            <Avatar src="/assets/images/avatar.png" sx={{ width: theme.spacing(12), height: theme.spacing(12) }} />
+          <Box width='100%' height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center'>
+            <Avatar src="assets/images/avatar.png" sx={{ width: theme.spacing(12), height: theme.spacing(12) }} />
           </Box>
 
           <Divider />
 
           <Box flex={1}>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Pagina inicial" />
-              </ListItemButton>             
+              {drawerOptions.map((drawerOption) => (
+                <ListItemLink
+                  key={drawerOption.path}
+                  icon={drawerOption.icon}
+                  label={drawerOption.label}
+                  to={drawerOption.path}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
+
             </List>
           </Box>
-
         </Box>
       </Drawer>
 
--- src/shared/contexts/DrawerContext.tsx
+++ src/shared/contexts/DrawerContext.tsx
@@ -1,10 +1,16 @@
-// Arquivo do contexto (AppThemeProvider.tsx)
 import { createContext, useCallback, useContext, useState } from "react";
 
+interface IDrawerOptions {
+  path: string;
+  label: string;
+  icon: string;
+}
 
 interface IDrawerContextData {
   isDrawerOpen: boolean;
+  drawerOptions: IDrawerOptions[];
   toggleDrawerOpen: () => void;
+  setDrawerOptions: (newDrawerOptions: IDrawerOptions[]) => void;
 }
 
 const DrawerContext = createContext<IDrawerContextData | undefined>(undefined);
@@ -12,7 +18,7 @@ const DrawerContext = createContext<IDrawerContextData | undefined>(undefined);
 export const useDrawerContext = () => {
   const context = useContext(DrawerContext);
   if (!context) {
-    throw new Error("useAppThemeContext must be used within a AppThemeProvider");
+    throw new Error("useDrawerContext must be used within a DrawerProvider");
   }
   return context;
 };
@@ -23,18 +29,19 @@ interface IDrawerProviderProps {
 
 export const DrawerProvider: React.FC<IDrawerProviderProps> = ({ children }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [drawerOptions, setDrawerOptions] = useState<IDrawerOptions[]>([]);
 
   const toggleDrawerOpen = useCallback(() => {
-    setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen);
+    setIsDrawerOpen((oldDrawerOpen) => !oldDrawerOpen);
   }, []);
 
-  
+  const handleSetDrawerOptions = useCallback((newDrawerOptions: IDrawerOptions[]) => {
+    setDrawerOptions(newDrawerOptions);
+  }, []);
 
   return (
-    <DrawerContext.Provider value={{isDrawerOpen, toggleDrawerOpen }}>      
-          {children}       
+    <DrawerContext.Provider value={{ isDrawerOpen, drawerOptions, toggleDrawerOpen, setDrawerOptions: handleSetDrawerOptions }}>
+      {children}
     </DrawerContext.Provider>
   );
 };
-
-
